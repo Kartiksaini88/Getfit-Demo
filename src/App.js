f90edd4a69fd8a32ed1c73f5cb61079c35e0048c
Kartik Saini <sainikartik82@gmail.com>
2022-07-13 23:32:14 +0530

    some work on reasons div
    working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Hero } from './components/hero/hero';
 import { Program } from './components/programs/program';
+import { Reasons } from './components/reasons/reasons';
 function App() {
   return (
     <div className="App">
           <Hero></Hero>
           <Program></Program>
+          <Reasons></Reasons>
     </div>
   );
 }
---
+++ src/components/reasons/reasons.jsx
@@ -0,0 +1,48 @@
+import './reasons.css'
+
+import image1 from '../../assets/image1.png'
+import image2 from '../../assets/image2.png'
+import image3 from '../../assets/image3.png'
+import image4 from '../../assets/image4.png'
+import nb from '../../assets/nb.png'
+import adidas from '../../assets/adidas.png'
+import nike from '../../assets/nike.png'
+import tick from '../../assets/tick.png'
+
+export const Reasons = ()=>{
+    return(
+        <div className="Reasons" id="reasons">
+            <div className="left-r">
+                <img src={image1} alt="" />
+                <img src={image2} alt="" />
+                <img src={image3} alt="" />
+                <img src={image4} alt="" />
+            </div>
+            <div className="right-r">
+                <span>Some Reasons</span>
+                <div>
+                    <span className='stroke-text'>Why </span>
+                    <span>choose us?</span>
+                </div>
+                <div className='details-r'>
+                    <div>
+                        <img src={tick} alt="" />
+                        <span>Over 140+ expert Coaches</span>
+                    </div>
+                    <div>
+                        <img src={tick} alt="" />
+                        <span>Train Smarter And Faster Than Before</span>
+                    </div>
+                    <div>
+                        <img src={tick} alt="" />
+                        <span>1 Free Program For New Member</span>
+                    </div>
+                    <div>
+                        <img src={tick} alt="" />
+                        <span>Reliable Partners</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
